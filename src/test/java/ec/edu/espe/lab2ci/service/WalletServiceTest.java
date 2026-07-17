333694546a51de7a988047ef6f6ec0d467aeafb0
Estiven Leonel Oña Chicaiza <elona@espe.edu.ec>
2026-07-17 16:14:23 -0500

    feat: añadir funcionalidad de withdraw con sus pruebas unitarias

--- src/test/java/ec/edu/espe/lab2ci/service/WalletServiceTest.java
+++ src/test/java/ec/edu/espe/lab2ci/service/WalletServiceTest.java
@@ -97,4 +97,25 @@ public class WalletServiceTest {
         Wallet saved = captor.getValue();
         Assertions.assertEquals(newBalance, saved.getBalance());
     }
+
+//    @Test
+//    void () {
+//
+//    }
+
+    @Test
+    void withdrawWithInsufficientFundsShouldThrowExceptionAndNotSave(){
+        // Arrange
+        Wallet wallet = new Wallet("estiven.ona@espe.edu.ec", 150.00);
+        String walletId = wallet.getId();
+
+        Mockito.when(walletRepository.findById(walletId)).thenReturn(Optional.of(wallet));
+
+        // Act + Assert
+        IllegalStateException ex = Assertions.assertThrows(IllegalStateException.class, () ->
+                walletService.withdraw(walletId, 1000.00));
+
+        Assertions.assertEquals("Insufficient funds", ex.getMessage());
+        Mockito.verify(walletRepository, Mockito.never()).save(ArgumentMatchers.any(Wallet.class));
+    }
 }
